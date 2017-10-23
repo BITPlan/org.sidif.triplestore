8fa304a92a392020bdfd993c46cf9b4cd2c01dd9
Wolfgang Fahl <wf@bitplan.com>
2017-10-23 07:17:47 +0200

    checks for readability of all examples

--- src/test/java/org/sidif/triplestore/TestTripleStream.java
+++ src/test/java/org/sidif/triplestore/TestTripleStream.java
@@ -20,32 +20,65 @@
  */
 package org.sidif.triplestore;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
+import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Stream;
-import java.util.stream.StreamSupport;
 
 import org.junit.Test;
 import org.sidif.parser.SiDIFLanguageParser;
 import org.sidif.triple.Triple;
 import org.sidif.util.SiDIFReader;
+import org.sidif.visitor.SiDIFTripleListVisitor;
 
 /**
  * test Triple Stream functions
+ * 
  * @author wf
  *
  */
-public class TestTripleStream {
+public class TestTripleStream extends BaseSiDIFTest {
+
+  /**
+   * check that the two triple list have the same content
+   * @param tripleList1
+   * @param tripleList2
+   */
+  public void checkEquals(List<Triple> tripleList1, List<Triple> tripleList2) {
+    // https://stackoverflow.com/a/34818800/1497139
+    Iterator<Triple> iter1 = tripleList1.iterator(),
+        iter2 = tripleList2.iterator();
+    while (iter1.hasNext() && iter2.hasNext())
+      assertEquals(iter1.next(), iter2.next());
+    assert !iter1.hasNext() && !iter2.hasNext();
+  }
 
   @Test
   public void testTripleStream() throws Exception {
-    SiDIFReader siDIFReader = new SiDIFLanguageParser();
-    File sidifFile=new File("src/test/resources/sidif/royal92.sidif");
-    List<Triple> tripleList = siDIFReader.fromSiDIFFile(sidifFile);
-    Stream<Triple> tripleStream = tripleList.stream();
-    assertEquals(31390,tripleStream.count());
+    /**
+     * example1 (10) example2 (15) familyTree (51) graph1 (7)
+     * json_ld_manu_sporny (16) notation3_TonyBenn (4) presentation (39) rdf_cd
+     * (22) rdf_json_anna_wilder (8) royal92-14 (209) royal92 (25614)
+     * trig_bob_alice (11) turtle_spiderman (8) typetest (62) utf8 (3) vcard
+     * (31)
+     */
+    int expectedSize[] = { 11, 15, 51, 7, 16, 4, 546, 22, 8, 210, 31390, 11, 8,
+        63, 3, 31 };
+    int index = 0;
+    for (String example : super.getAllExampleNames()) {
+      File sidifFile = this.getExampleFile(example);
+      SiDIFReader siDIFReader = new SiDIFLanguageParser();
+      SiDIFReader siDIFReader2 = new SiDIFTripleListVisitor();
+      List<Triple> tripleList = siDIFReader.fromSiDIFFile(sidifFile);
+      List<Triple> tripleList2 = siDIFReader2.fromSiDIFFile(sidifFile);
+      Stream<Triple> tripleStream = tripleList.stream();
+      Stream<Triple> tripleStream2 = tripleList2.stream();
+      assertEquals(""+index+":"+example,expectedSize[index], tripleStream.count());
+      assertEquals(""+index+":"+example,expectedSize[index], tripleStream2.count());
+      // checkEquals(tripleList,tripleList2);
+      index++;
+    }
   }
-
 }
